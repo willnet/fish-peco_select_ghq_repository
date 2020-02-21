c89222109d4c0aadbce8a7787800ad80a12ccde0
willnet <netwillnet@gmail.com>
2020-02-21 16:12:35 +0900

    Fix something wrong with latest peco and ghq

--- functions/peco_select_ghq_repository.fish
+++ functions/peco_select_ghq_repository.fish
@@ -1,11 +1,5 @@
 function peco_select_ghq_repository
-  set -l query (commandline)
-
-  if test -n $query
-    set peco_flags --query "$query"
-  end
-
-  ghq list | peco $peco_flags | read line
+  ghq list | peco | read line
 
   if [ $line ]
     cd (ghq list --full-path --exact $line)
